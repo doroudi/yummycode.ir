a0d63ef87cf7586fa0de32b2f9b9c9a97667fd08
Saeid Doroudi <doroudi@live.com>
2020-09-04 13:07:47 +0430

    add new post powershell script

---
+++ NewPost.ps1
@@ -0,0 +1,55 @@
+Param (
+    [Parameter(Mandatory = $true, HelpMessage = "(Required) The title of the post.")]
+    [string] $Title,
+    [Parameter(Mandatory = $true, HelpMessage = "(Required) The title of the post.")]
+    [string] $Description,
+    [Parameter(Mandatory = $false, HelpMessage = "(Optional) The author of the post.")]
+    [AllowEmptyString()]
+    [string] $Author,
+    [Parameter(Mandatory = $false, HelpMessage = "(Optional) The name of the series of posts.")]
+    [AllowEmptyString()]
+    [string] $Series,
+    [Parameter(Mandatory = $false, HelpMessage = "(Optional) The order of the series of posts.")]
+    [AllowEmptyString()]
+    [int] $SeriesOrder)
+
+$Author = 'Saeid Doroudi';
+if ('' -ne $Series) {
+    $Series =
+"`nseries: ""$($Series)""
+seriesOrder: $($SeriesOrder)"
+}
+
+$date = Get-Date;
+$year = $date.Year;
+$month = $date.Month;
+$timestamp = $date.ToString('yyyy-MM-ddTHH:mm:ssZ');
+$slug = $Title.Replace(' ', '-').ToLowerInvariant();
+$permalink = '/' + $slug + '/';
+
+$content =
+"---
+title: ""$($Title)""
+description: ""$($Description)""
+author: ""$($Author)""
+permalink: ""$($permalink)""
+heroImage: ""/images/hero/Microsoft-.NET-1366x768.png""
+date: ""$($timestamp)""
+dateModified: null
+published: false$($Series)
+categories:
+  - """"
+tags:
+  - """"
+---
+
+";
+
+Write-Output $content;
+
+$postDirectoryPath = Join-Path $PSScriptRoot "\content\posts\$($year)\$($month)\$($slug)";
+$imagesDirectoryPath = Join-Path $postDirectoryPath "\images";
+$markdownFilePath = Join-Path $postDirectoryPath "index.md";
+
+New-Item -ItemType Directory -Force -Path $imagesDirectoryPath;
+Set-Content -Path $markdownFilePath -Value $content;
